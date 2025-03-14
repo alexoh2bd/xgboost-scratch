765c0351f8fb080eba5946294f14347039485eeb
alexoh2bd <alexoh2020@gmail.com>
2025-03-13 17:32:22 -0700

    Updated predictions/probabilities step, added Visualization with matplotlib

--- Trees/TreesFromScratch/modeling/xgb2.py
+++ Trees/TreesFromScratch/modeling/xgb2.py
@@ -2,13 +2,20 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
-from math import e 
+from math import e, log
 
 import typer
 from loguru import logger
 from tqdm import tqdm
 
 from TreesFromScratch.config import MODELS_DIR, PROCESSED_DATA_DIR
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.datasets import make_regression
+
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
 
 
 
@@ -33,8 +40,11 @@ class Node():
         self.thresh = None # If not leaf, threshold
         self.leftNode = None
         self.rightNode = None
+        self.prob = None
     def setSplit(self,split):
         self.split=split
+    def setProb(self, prob):
+        self.prob = prob
     
 class Tree():
     '''
@@ -43,7 +53,7 @@ class Tree():
         maxDepth
         min_samples_split
     '''
-    def __init__(self, probs, y, x, maxDepth=5, min_cover =1, lam=1, gamma=1,lr=0.3):
+    def __init__(self, probs, x, y, maxDepth=5, min_cover =1, lam=1, gamma=1,lr=0.3):
         self.y = y                  # predicting class
         self.x = x                  # feature class
         self.rootNode = None    # comes with data
@@ -60,6 +70,7 @@ class Tree():
         self.hessians = [(prob * (1-prob)) for prob in probs]
     
     def simScore(self, resSum: float, hessSum: float) -> float:
+        assert ((hessSum) + self.lam) != 0
         return ((resSum)**2)/ ((hessSum) + self.lam)
     
     # find Split by maximizing Gain
@@ -75,9 +86,7 @@ class Tree():
         tempGain = 0
         rootSim = self.simScore(rRSum, rHSum)
         splitIdx = 0
-        # maxLSim=0
-        # maxRSim=0
-
+        
         # find max Gain to return Split
         for i in range(idx0, idx1):
             lRSum += self.residuals[i]
@@ -98,14 +107,12 @@ class Tree():
 
     # values are 0 or 1, predictions are 0-1 probabilities
     def buildBranch(self, node: Node, idx0, idx1, currdepth:int):
-        # If we reach the end, turn into a leaf and return
+        # If we reach max depth or cover < minimum allowed (prune), turn into a leaf
         assert isinstance(node, Node) 
         cover = sum(self.hessians[idx0:idx1])
-        if  cover<= self.min_cover:
+        if cover<= self.min_cover or currdepth>=self.maxDepth:
             node.isLeaf = True
-            return
-        if currdepth>=self.maxDepth:
-            node.isLeaf = True
+            node.prob=self.probs[idx0]
             return
         
         split = self.findSplit(idx0, idx1)
@@ -123,23 +130,82 @@ class Tree():
 
     
     def buildTree(self, features: list):
-        self.rootNode = Node(features[0], 0, self.len)
-        self.buildBranch(self.rootNode, 0, self.len, 0) 
-        # then prune 
-        # then update probabilities, return new predictions
+        '''
+        Build new tree for each iteration, update probabilities(predictions) after each tree
+        '''
+        for i in range(10):
+            print(f"Iteration {i}")
+            self.rootNode = Node(features[0], 0, self.len)
+            self.buildBranch(self.rootNode, 0, self.len, 0) 
+            # then update probabilities, return new predictions
+            self.updateProbs(currnode=self.rootNode, depth=0)
         self.visualizeTree(self.rootNode, 0)
 
 
-    def visualizeTree(self, currnode, depth):
-        print(f"{'  '*depth}Node Depth: {depth}. Split Index: {currnode.split}. Bounds: {currnode.idx0} - {currnode.idx1}. ")
+    # For each leaf, calculate new output value, then update all probs
+    def updateProbs(self, currnode, depth):
+        print(f"{'  '*depth}Node Depth: {depth}. Threshold: {currnode.thresh} ")
         if currnode.isLeaf and currnode.leftNode is None and currnode.rightNode is None:
-            print(f"{'  '*depth}Leaf!")
+            i, j = currnode.idx0, currnode.idx1
+            assert (sum(self.hessians) + self.lam) != 0
+            output = sum(self.residuals[i:j])/(sum(self.hessians[i:j]) + self.lam)
+            logodd = log(currnode.prob/ (1-currnode.prob))
+            logpred =  logodd + self.lr * output  # Log(odds) prediction 
+            self.probs[i:j] = [e**logpred/ (1+e**logpred) for _ in range(i,j)]
+            currnode.prob=self.probs[i] 
+            print(f"{'  '*depth}LEAF! Bounds: {i} - {j}.")
+            print(f"{'  '*depth}  Output: {output}. Log(Odds): {logodd}.")
+            print(f"{'  '*depth}  Log(odds) Prediction: {logpred}. Probs: {self.probs[i:i+10]}.")
             return
         if currnode.leftNode:
-            self.visualizeTree(currnode.leftNode, depth+1)
+            self.updateProbs(currnode.leftNode, depth+1)
         if currnode.rightNode:
-            self.visualizeTree(currnode.rightNode, depth+1)
+            self.updateProbs(currnode.rightNode, depth+1)
 
+    def extractThresh(self):
+        # Initialize lists for each feature
+        tree_thresholds = [min(self.x),max(self.x)]
+        tree_probs =[]
+        bounds=[]
+        
+        # Queue for traversing the tree (BFS) starting with root
+        queue = [self.rootNode]
+        
+        while queue:
+            node = queue.pop(0)
+            
+            # Note leaf node probs
+            if node.isLeaf:
+                tree_probs.append(node.prob)
+                bounds.append([node.idx0, node.idx1])
+                continue
+            tree_thresholds.append(node.thresh)
+
+            if node.leftNode:
+                queue.append(node.leftNode)
+            if node.rightNode:
+                queue.append(node.rightNode)
+
+        return tree_thresholds, tree_probs, bounds
+
+    def visualizeTree(self, currnode, depth):
+        '''
+        Visualize Predicted Probabilities in data
+        '''
+        fig, ax = plt.subplots(figsize=(10, 8))
+        scatter = ax.scatter(self.x, self.y, c=self.y, cmap='viridis', alpha=0.6, edgecolors='w', s=30)
+        thresholds, probs, bounds = self.extractThresh()
+        
+        thresholds = (np.ravel(thresholds))
+        thresholds.sort()
+        print(f'thresholds: {thresholds}.  probs:{probs}.  bounds: {bounds}')
+        for b in thresholds:
+            ax.axvline(x=b, color='r', linestyle='-',  linewidth=1)
+        for i in range(len(probs)):
+            ax.hlines(y=probs[i], xmin=thresholds[i], xmax=thresholds[i+1], color='b', linestyle='-', linewidth=1)
+    
+        plt.tight_layout()
+        plt.show()
 
 
 
@@ -198,14 +264,28 @@ def main(
     # print((age[:10]))
     probs = [0.5 for i in range(len(age))]
     residuals = [(transported[i] - probs[i]) for i in range(len(transported))]
-    root = Node('Age', residuals,probs)
-    tree = Tree(probs, transported, age, min_cover=300)
+    tree = Tree(probs, age, transported, min_cover=300)
     features= ['Age']
     tree.buildTree(features)
-    # print(transported)
-    # logger.info(transported[:5])
-    # logger.info(age[:5])
-    # logger.info(len(probs))
+
+
+    
+    # np.random.seed(42)
+    # n_samples = 500
+    # noise=1
+    # X = np.random.uniform(0, 10, (n_samples))
+    # # Create a simple decision boundary at x = 5
+    # y = (X > 5).astype(int)
+    # noise_mask = np.random.random(n_samples) < noise
+    # y[noise_mask] = 1 - y[noise_mask]
+    
+    # probs = [0.5 for i in range(len(X))]
+    
+    # tree = Tree(probs, X, y, min_cover=50)
+    # features= ['X']
+    # tree.buildTree(features)
+
+
 
 if __name__ == "__main__":
     app()
